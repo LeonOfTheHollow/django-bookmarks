a493a86f86b15a357899227c813818249af0bf7b
Russell Leon Bates IV <russell.l.bates@gmail.com>
2018-05-24 15:57:07 -0700

    Got (Personal)Bookmarks running. Working on csrf

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User
 
 class Bookmark(models.Model):
   id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
   url = models.URLField('URL', unique=True)
   name = models.CharField(max_length=200)
   notes = models.TextField(blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  last_modified = models.DateTimeField(auto_now=True)
+
+class PersonalBookmark(Bookmark):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from .models import Bookmark, PersonalBookmark
+from .forms import BookmarkForm
 # Create your views here.
+
+def index(request):
+  context = {
+    'bookmarks': Bookmark.objects.all(),
+  }
+
+  pbid = PersonalBookmark.objects.values_list('id')
+
+  context['bookmarks'] = Bookmark.objects.exclude(id__in=pbid)
+
+  if request.user.is_anonymous:
+    context['personal_bookmarks'] = PersonalBookmark.objects.none()
+  else:
+    context['personal_bookmarks'] = PersonalBookmark.objects.filter(user=request.user)
+
+  context['form'] = BookmarkForm
+
+  return render(request, 'bookmarks/index.html', context)
